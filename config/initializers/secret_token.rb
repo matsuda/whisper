111e7914bbd1373f035abeba5b0b6068c6842d38
Kosuke Matsuda <kosukematsuda@gmail.com>
2011-04-23 23:21:34 +0900

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slnt::Application.config.secret_token = '546d4ebaf8653b21eaeb7f92d699e321be44fc0b19b2d8e7b5f6bc2563ce7365e978719a20ba4927c0d6770a942d5908912ac7e647a547df4eb9161f7dc374f5'
